3a8e32d96b0bb92c268eb03647b395d2c51dbd90
Olivier Burri <olivier.burri@gmail.com>
2019-11-04 09:25:19 +0100

    adds new example script

--- Scripts/OperettaExportDataForMachineLearning.groovy
+++ Scripts/OperettaExportDataForMachineLearning.groovy
@@ -1,5 +1,7 @@
 //#@File id (label="Selected File")
 //#@File save_dir (label="Save Location", style="directory")
+//#@String dataset_name (label="Dataset Name", style="directory")
+
 //#@String sources_str (label="Source Channels", value="1")
 //#@String targets_str (label="Target Channels", value="2")
 
@@ -12,42 +14,45 @@ import ij.ImagePlus
 import ij.plugin.ChannelSplitter
 import ij.plugin.RGBStackMerge
 
-// Create the directories
-def source_dir = new File ( save_dir, 'source' )
-def target_dir = new File ( save_dir, 'target' )
-
-source_dir.mkdirs()
-target_dir.mkdirs()
-
 
 // Start the Operetta Manager
 def opm = new OperettaManager.Builder( )
-                            .setId( id )
-                            .setSaveFolder( save_dir )
-                            .build( )
+		.setId( id )
+		.setSaveFolder( save_dir )
+		.build( )
 
 // Pick up the channels to use as source
 def sources = HyperRange.parseString( sources_str )
 def targets = HyperRange.parseString( targets_str )
 
 
-println( sources )
-
-
-
 def allWells = opm.getAvailableWells( )
+
+// Grab pixel size
+def cal = opm.getCalibration()
+
+// Round the calibration
+def rounded_px_size = cal.pixelWidth.round(2)
+
+
+// Create the directories
+def source_dir = new File ( save_dir, "source/" + dataset_name + "-"+rounded_px_size )
+def target_dir = new File ( save_dir, "target/" + dataset_name + "-"+rounded_px_size )
+
+source_dir.mkdirs()
+target_dir.mkdirs()
+
 
 //Process all wells
 
 allWells.each{ well ->
-    def fields = opm.getAvailableSamples( well )
-	
-	
-GParsPool.withPool( 6 ) {
-	fields.eachParallel { field ->
+	def fields = opm.getAvailableSamples( well )
+
+
+	fields.each { field ->
 		def image = opm.getFieldImage( field )
 		//image.show()
-		
+
 		ImagePlus[] channels = ChannelSplitter.split( image )
 
 		def source_images = sources.collect{ return channels[ it-1 ] }
@@ -55,19 +60,20 @@ GParsPool.withPool( 6 ) {
 
 		def source = source_images[0]
 		def target = target_images[0]
-		
+
 		if (source_images.size() > 1 )
 			source = RGBStackMerge.mergeChannels( source_images as ImagePlus[], false )
 
 		if (target_images.size() > 1 )
 			target = RGBStackMerge.mergeChannels( target_images as ImagePlus[], false )
 
-		def hash = image.getTitle().digest('SHA-1')
+		//def hash = image.getTitle().digest('SHA-1')
+
+		def hash = image.getTitle()
 
 		IJ.saveAsTiff( source, new File( source_dir, hash ).getAbsolutePath() )
 
 		IJ.saveAsTiff( target, new File( target_dir, hash ).getAbsolutePath() )
 		println ( sprintf( "Field %s done, saved as %s", image.getTitle(), hash ) )
 	}
-}
 }
