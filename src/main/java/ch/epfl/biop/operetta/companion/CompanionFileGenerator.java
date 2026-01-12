fe4c7216f429f037cde9cfeabfb01b23c19783bb
Rémy Dornier <remy.dornier@epfl.ch>
2026-01-12 10:43:09 +0100

    fix licences

--- src/main/java/ch/epfl/biop/operetta/companion/CompanionFileGenerator.java
+++ src/main/java/ch/epfl/biop/operetta/companion/CompanionFileGenerator.java
@@ -4,7 +4,7 @@ package ch.epfl.biop.operetta.companion;
  * #%L
  * Hold your horses
  * %%
- * Copyright (C) 2019 - 2025 BIOP
+ * Copyright (C) 2019 - 2026 BIOP
  * %%
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as
